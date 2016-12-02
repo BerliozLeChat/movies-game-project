eb558efb66a121b35924b7c4f2e366480fd5ae6d
Camille <Camille>
2016-12-02 15:08:28 +0100

    Correction google auth

--- movies-game/src/main/webapp/game/app.js
+++ movies-game/src/main/webapp/game/app.js
@@ -1,6 +1,16 @@
 (function() {
+	var CLIENT = '537596784004-7k1ehu9p3vp7b0kqc5691tkopvlmti3g.apps.googleusercontent.com';
 
-    var app = angular.module('movies-game', ['ngMap']);
+	var app = angular.module('movies-game',['angular-google-gapi','ngCookies', 'ngMap']);
+
+	app.run(['GAuth', function(GAuth) {
+
+		GAuth.setClient(CLIENT);
+		GAuth.setScope('https://www.googleapis.com/auth/userinfo.profile');
+
+		GAuth.load();
+
+	}]);
 
     app.run(function($rootScope) {
         $rootScope.$on('mapInitialized', function(evt,map) {
@@ -291,11 +301,39 @@
         }
 
     ]);
-    app.run(function($rootScope) {
-        $rootScope.$on('mapInitialized', function(evt,map) {
-            $rootScope.map = map;
-            $rootScope.$apply();
-        });
-    })
+	
+	app.controller('MainController', ['$scope','GAuth','$cookies','GData', function($scope, GAuth, $cookies, GData) {
 
+		$scope.user = null;
+
+		if ($cookies.get("google_id")) {
+			GData.setUserId($cookies.get("google_id"));
+
+			GAuth.checkAuth().then(
+				function(user) {
+					$scope.user = user;
+				},
+				function() {
+					console.log("Oups! Failure to connect...");
+				}
+			)
+		}
+
+		$scope.login = function() {
+			GAuth.login().then(function(user) {
+				console.log(user);
+				$scope.user = user;
+				$cookies.put("google_id", user.id);
+			}, function() {
+				console.log("Oups! Failure to connect...");
+			});
+		};
+
+		$scope.logout = function() {
+			GAuth.logout();
+			$scope.user = null;
+			$cookies.remove("google_id");
+		};
+
+	}]);
 })();
