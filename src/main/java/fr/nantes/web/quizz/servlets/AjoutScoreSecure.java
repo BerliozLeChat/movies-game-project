d94d3eac7a86e3c6ae5a3ac161a09bbfc369c4a8
SacreDeBirmanie <sebful44@hotmail.fr>
2016-12-13 19:05:40 +0100

    modification de l'envoi des scores pour des tests

--- src/main/java/fr/nantes/web/quizz/servlets/AjoutScoreSecure.java
+++ src/main/java/fr/nantes/web/quizz/servlets/AjoutScoreSecure.java
@@ -27,15 +27,15 @@ public class AjoutScoreSecure extends HttpServlet {
             if(Requetesdatastore.existScoreUser(user.getUserId()))
                 try {
                     if (Requetesdatastore.updateExistingScore(user.getUserId(), (int) request.getAttribute("score")))
-                        response.setStatus(251, "Félicitation, vous avez battu votre meilleur score !");
+                        response.setStatus(251); //"Félicitation, vous avez battu votre meilleur score !");
                     else
-                        response.setStatus(250, "Vous n'avez pas battu votre meilleur score, essayez encore ;)");
+                        response.setStatus(250 );//"Vous n'avez pas battu votre meilleur score, essayez encore ;)");
                 }catch (EntityNotFoundException e){
                     response.sendError(500, "erreur de fonctionnement interne");
                 }
             else{
-                    Requetesdatastore.insertNewScore(user.getUserId(),user.getNickname(),(int) request.getAttribute("score"));
-                    response.setStatus(201,"Merci d'avoir jouer, votre score est enregistré. Vous pouvez continuer à vous amuser :)");
+                    Requetesdatastore.insertNewScore(user.getUserId(),user.getNickname(),Integer.parseInt(request.getParameter("score")));
+                    response.setStatus(201);//"Merci d'avoir jouer, votre score est enregistré. Vous pouvez continuer à vous amuser :)");
 
             }
 
